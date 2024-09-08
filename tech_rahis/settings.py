806ccce02b4f518a62297605d79c4458ca4e64a3
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-09-08 13:11:07 +0530

    Update .gitignore and project configuration files

--- tech_rahis/settings.py
+++ tech_rahis/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['techrahis.vercel.app','techrahis.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
