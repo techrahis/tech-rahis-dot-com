22c23739f6c05bf2585dbd64ca09fe765dbbd132
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-09-19 13:42:06 +0530

    Refactor templates and views for "Get Free 1:1 Consultation" feature Refactor meta description in about and contact templates Refactor Dockerfile to improve build and deployment process Refactor email address and button styling in templates Update header_list.html to change "Contact" to "Services" Update mobile_header_list.html to change button text to "⚡ Get Free 1:1 Consultation" Update navbar.html to change button text to "⚡ Get Free 1:1 Consultation" Update footer.html to add "Contact" link Update blog_detail.html to extend base template and add blog details Update views.py to refactor blog_list and add blog_detail

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('search/', views.blog_search, name='blog_search'),
-    path('category/<slug:slug>/', views.blog_category, name='blog_category'),
-    path('tag/<slug:slug>/', views.blog_tag, name='blog_tag'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,34 +1,55 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Blog, BlogCategory, Tag
+import markdown
 
 def blog_list(request):
+    query = request.GET.get('q', '')
+    category_slug = request.GET.get('category')
+    tag_slug = request.GET.get('tag')
+    
     blogs = Blog.objects.all()
-    paginator = Paginator(blogs, 10)  # Show 10 blogs per page
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'blog_list.html', {'page_obj': page_obj})
 
-def blog_search(request):
-    query = request.GET.get('q')
-    blogs = Blog.objects.filter(title__icontains=query) if query else Blog.objects.all()
-    paginator = Paginator(blogs, 10)  # Show 10 blogs per page
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'blog_list.html', {'page_obj': page_obj, 'query': query})
+    # Filter by query (search)
+    if query:
+        blogs = blogs.filter(title__icontains=query).only('id', 'title', 'short_description', 'thumbnail', 'slug').order_by('-date')
 
-def blog_category(request, slug):
-    category = get_object_or_404(BlogCategory, slug=slug)
-    blogs = Blog.objects.filter(category=category)
-    paginator = Paginator(blogs, 10)  # Show 10 blogs per page
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'blog_list.html', {'page_obj': page_obj, 'category': category})
+    # Filter by category
+    if category_slug:
+        category = get_object_or_404(BlogCategory, slug=category_slug)
+        blogs = blogs.filter(category=category).only('id', 'title', 'short_description', 'thumbnail', 'slug').order_by('-date')
 
-def blog_tag(request, slug):
-    tag = get_object_or_404(Tag, slug=slug)
-    blogs = Blog.objects.filter(tags=tag)
-    paginator = Paginator(blogs, 10)  # Show 10 blogs per page
+    # Filter by tag
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        blogs = blogs.filter(tags=tag).only('id', 'title', 'short_description', 'thumbnail', 'slug').order_by('-date')
+    
+    # Pagination
+    paginator = Paginator(blogs, 5)  # Show 10 blogs per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'blog_list.html', {'page_obj': page_obj, 'tag': tag})
+    
+    # Fetch all categories and tags for dynamic rendering
+    categories = BlogCategory.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'page_obj': page_obj,
+        'query': query,
+        'categories': categories,
+        'tags': tags,
+        'category': category if category_slug else None,
+        'tag': tag if tag_slug else None,
+    }
+    return render(request, 'blog_list.html', context)
+
+
+def blog_detail(request, slug):
+    # Get the blog by slug or return a 404 if not found
+    blog = get_object_or_404(Blog, slug=slug)
+    blog.long_description = markdown.markdown(blog.long_description, extensions=['extra'])
+    # Pass the blog object to the template
+    context = {
+        'blog': blog,
+    }
+    return render(request, 'blog_detail.html', context)
